10437294a252a57e3d12640d4b0e31ec1e48ab55
BrantKijin <upper0house@gmail.com>
2024-02-28 00:07:26 +0900

    init

--- coupon-core/src/main/java/com/fastcampus/couponcore/CouponCoreConfiguration.java
+++ coupon-core/src/main/java/com/fastcampus/couponcore/CouponCoreConfiguration.java
@@ -1,8 +1,10 @@
 package com.fastcampus.couponcore;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableCaching
 @ComponentScan
 @EnableAutoConfiguration
 public class CouponCoreConfiguration {
